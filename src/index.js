ff8f555251a576be8fa8efe4598f8cee51fdfa92
pablitomillenios <pescaida@gmail.com>
2025-09-19 18:39:30 +0200

    compiled

---
+++ src/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Number
+let age = 25;
+//String
+let name = "Alice";
+//Boolean
+let isStudent = true;
+//Array
+let numbers = [1, 2, 3];
+let names = ["Alice", "Bob", "Charlie"];
+//Tuple
+let person = ["Alice", 25];
+//any
+let anything = "Hello";
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+//Number
+let age: number = 25;
+
+//String
+let name: string = "Alice";
+
+//Boolean
+let isStudent: boolean = true;
+
+//Array
+let numbers: number[] = [1, 2, 3];
+let names: Array<string> = ["Alice", "Bob", "Charlie"];
+
+//Tuple
+let person: [string, number] = ["Alice", 25];
+
+//any
+let anything: any = "Hello";
+
